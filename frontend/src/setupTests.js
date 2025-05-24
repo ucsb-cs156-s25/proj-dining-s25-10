f05543d756aba52cce8bd9e4c43c5ff8a66b323c
Benjamin Soo <benjaminsoo@ucsb.edu>
2025-05-23 17:14:42 -0700

    ran format

--- frontend/src/setupTests.js
+++ frontend/src/setupTests.js
@@ -4,10 +4,10 @@
 // learn more: https://github.com/testing-library/jest-dom
 import "@testing-library/jest-dom";
 
-// To Fix Test Errors for React Router v7. 
-// Explanation: React Router v7 uses TextEncoder/TextDecoder APIs that are available in browsers but not globally available in Node.js test environments. 
-// This codeblock imports them from Node's util module and makes them globally accessible. 
-// Without this, tests fail with "TextEncoder is not defined" errors when importing React Router components. 
+// To Fix Test Errors for React Router v7.
+// Explanation: React Router v7 uses TextEncoder/TextDecoder APIs that are available in browsers but not globally available in Node.js test environments.
+// This codeblock imports them from Node's util module and makes them globally accessible.
+// Without this, tests fail with "TextEncoder is not defined" errors when importing React Router components.
 // This was not needed in v6 because the library didn't depend on these Web APIs.
 const { TextEncoder, TextDecoder } = require("util");
 
